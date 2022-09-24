d12cc1a56a35cfc06e46927428e25bf542889a66
murayama <murayama@m-lic.co.jp>
2022-09-24 14:49:53 +0900

    new

--- index.js
+++ index.js
@@ -19,15 +19,14 @@ $(function () {
 
     // 送信
     $('form').submit(function () {
-        var date = $('input[name="date"]').val();
         var number = $('input[name="number"]:checked').val();
-        var names = '';
+        var comment = '';
         $('#form-name').children().each(function (i, elm) {
-            names += $(elm).val() + '、';
+            comment += $(elm).val() + '、';
         })
-        names = names.slice(0, -1);
+        comment = comment.slice(0, -1);
 
-        var msg = `希望日：${date}\n人数：${number}\n氏名：${names}`;
+        var msg = `癒されたにゃ!!\nお気に入り度：${number}にゃ!\nコメント：${comment}`;
         sendText(msg);
 
         return false;
